a43533c7c4dc0bf9166f81c3f0916bfa67e8f9ee
dmitri <dymkatrakee@gmail.com>
2022-05-12 09:44:19 +0300

    Initial Commit

---
+++ ExuoSpaceX/ExuoSpaceXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExuoSpaceXApp.swift
+//  ExuoSpaceX
+//
+//  Created by Dumitru Paraschiv on 12.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct ExuoSpaceXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
